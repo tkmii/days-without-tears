91ed3744d5c585402f33fa727a4c4a4df19389e3
Tatiana <tanya0fr@gmail.com>
2023-11-27 19:17:58 +0400

    Доделала логику вывода свойства disabled у кнопок

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ Vue.createApp({
     return {
       count: localStorage.getItem('count') || 0,
       popupPositive: false,
-      isDisabled: false,
+      isDisabled: false || localStorage.getItem('isDisabled'),
       popupNegative: false,
       counter: []
     };
@@ -13,13 +13,21 @@ Vue.createApp({
       this.count++;
       localStorage.setItem('count', this.count);
       this.popupPositive = true;
+      localStorage.setItem('isDisabled', true);
       this.isDisabled = true;
+      setTimeout(() => {
+        this.isDisabled = false;
+      }, 86400000);
     },
     addNull() {
       this.count = 0;
       localStorage.setItem('count', this.count);
       this.popupNegative = true;
+      localStorage.setItem('isDisabled', true);
       this.isDisabled = true;
+      setTimeout(() => {
+        this.isDisabled = false;
+      }, 86400000);
     },
   },
 }).mount('#app')
