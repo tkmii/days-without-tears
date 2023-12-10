87165f66828c1cdf9581b07570c65e7c37c1dbfb
Tatiana <tanya0fr@gmail.com>
2023-12-10 20:32:20 +0400

    Поменяла дизайн кнопок, сделала адаптив, добавила фавикон

--- scripts.js
+++ scripts.js
@@ -10,9 +10,33 @@ Vue.createApp({
       positiveImages: ['images/cat.jpg', 'images/cats1.jpg', 'images/cat2.jpg', 'images/cats3.jpg', 'images/cat4.jpg', 'images/cats5.jpg'],
       memeImages: ['images/meme.jpg', 'images/meme1.jpg', 'images/meme2.jpg', 'images/meme3.jpg', 'images/meme4.jpg', 'images/meme5.jpg'],
       quote: '',
-      quotes: ['Не бывает безвыходных ситуаций. Бывают ситуации, выход из которых тебя не устраивает.', 'Никогда не сдаваться... Встать, когда все рухнуло — вот настоящая сила.', 'Нельзя выиграть, если ты только защищаешься. Чтобы выиграть, нужно идти в атаку.', 'Опыт приходит с болью.', 'Не кипешуй. Паника приводила к гибели и опытных людей.', 'Ошибки не оковы, не дающие сделать шаг вперед. Благодаря им дух становится сильнее.'],
-      author: '',
-      authors: ['Нара Шикамару', 'Наруто Узумаки', 'Лайт Ягами', 'Себастьян Михаэлис', 'Доппо Куникида', 'Кана Альберона']
+      autor: '',
+      quotes: [
+        {
+          name: 'Нара Шикамару',
+          cite: 'Не бывает безвыходных ситуаций. Бывают ситуации, выход из которых тебя не устраивает.'
+        },
+        {
+          name: 'Наруто Узумаки',
+          cite: 'Никогда не сдаваться... Встать, когда все рухнуло — вот настоящая сила.'
+        },
+        {
+          name: 'Лайт Ягами',
+          cite: 'Нельзя выиграть, если ты только защищаешься. Чтобы выиграть, нужно идти в атаку.'
+        },
+        {
+          name: 'Себастьян Михаэлис',
+          cite: 'Опыт приходит с болью.'
+        },
+        {
+          name: 'Доппо Куникида',
+          cite: 'Не кипешуй. Паника приводила к гибели и опытных людей.'
+        },
+        {
+          name: 'Кана Альберона',
+          cite: 'Ошибки не оковы, не дающие сделать шаг вперед. Благодаря им дух становится сильнее.'
+        }
+      ]
     };
   },
   methods: {
@@ -20,7 +44,7 @@ Vue.createApp({
       this.count++;
       localStorage.setItem('count', this.count);
       this.popupPositive = true;
-     localStorage.setItem('isDisabled', true);
+      localStorage.setItem('isDisabled', true);
       this.isDisabled = true;
       
       setTimeout(() => {
@@ -54,8 +78,8 @@ Vue.createApp({
 
       this.memeSrc = this.memeImages[randomNum];
 
-      this.quote = this.quotes[randomNum];
-      this.author = this.authors[randomNum];
+      this.quote = this.quotes[randomNum].cite;
+      this.author = this.quotes[randomNum].name;
       return this.memeSrc, this.quote, this.author
     },
   },
